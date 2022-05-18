1ee7769f7147f87f6e8f7166ef4e7928845f5b56
Rodrigo Zafra <rodrigo.zafra@radmas.com>
2022-05-18 17:43:51 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, DoCheck } from '@angular/core';
+import { User } from './models/user';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
-export class AppComponent {
+export class AppComponent implements OnInit, DoCheck {
   title = 'videos-angular';
+  public identity: any;
+  public token: any;
+
+  constructor(private _userService: UserService) {
+
+  }
+  ngDoCheck(): void {
+    this.loadUser(); 
+  }
+  ngOnInit(): void {
+
+  }
+
+  loadUser() {
+    this.identity = this._userService.getIdentity();
+    this.token = this._userService.getToken();
+  }
 }
